639be14ed41831dcd0d9a67834c914ae33461ed5
Subathra R <subathrar2005@gmail.com>
2026-04-19 22:17:56 +0530

    Fixed backend for render

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5001;
-console.log(PORT);
 app.listen(PORT, '0.0.0.0', () => {
 console.log(`Server is running at: http://localhost:${PORT}`);
 });
